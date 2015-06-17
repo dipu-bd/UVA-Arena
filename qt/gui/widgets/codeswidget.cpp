1c0f1b187fed85032edd37fadd79baf262aca689
jgcoded <juliog@knights.ucf.edu>
2015-06-16 22:11:53 -0400

    Committing in QScintilla to the project. I'd rather not have contributors downloading all the dependencies of the project. I also had to modify the Qt related build files for QScintilla so it would integrate nicely with the build infrastructure of UVA-Arena.

--- qt/gui/widgets/codeswidget.cpp
+++ qt/gui/widgets/codeswidget.cpp
@@ -1,11 +1,13 @@
 #include "codeswidget.h"
 #include "ui_codeswidget.h"
+#include <Qsci/qsciscintilla.h>
 
 CodesWidget::CodesWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::CodesWidget)
 {
     ui->setupUi(this);
+
 }
 
 CodesWidget::~CodesWidget()

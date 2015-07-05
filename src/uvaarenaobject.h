31539a86735fcfa4e6b9b77187e0befd7fcdb02f
jgcoded <juliog@knights.ucf.edu>
2015-07-04 20:16:08 -0400

    uvaarenaobject: initial commit

---
+++ src/uvaarenaobject.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <QObject>
+
+namespace uva
+{
+	
+	/**
+		\brief base class for all UVA-Arena widgets. This class facilitates
+			   communication between widgets through signals and slots, and by providing
+			   methods to access common functionality.
+	*/
+	class UVAArenaObject : public QObject
+	{
+		Q_OBJECT
+
+		public:
+
+		private:
+
+	};
+
+}

ddbc7f6c553d9247e227d9ca359c5979fdb83535
dipu-bd <dipu.sudipta@gmail.com>
2015-07-08 14:48:45 +0600

    Created getJudgeStatus() method. Used QJsonDocument to parse data in problemListFromData().

--- tests/uhuntqttest.cpp
+++ tests/uhuntqttest.cpp
@@ -1,50 +1,83 @@
+#include <stdio.h>
 #include <iostream>
 #include <memory>
 #include <QApplication>
-#include <QNetworkAccessManager>
 #include <QPushButton>
+#include <QNetworkAccessManager>
+
 #include "uhunt/uhuntqt.h"
 
 using namespace std;
 
-const QByteArray sampleData = "[[40,104,\"this \\\"is\\\" [a] test\",3711,10,1000000000,0,306,0,0,2238,0,2205,5,3538,427,12052,578,6228,3000,2,0],[36,100,\"The 3n + 1 problem\",69204,7,1000000000,0,6669,0,0,102913,0,57933,146,53082,5209,242535,4512,174208,3000,1,0],[37,101,\"The Blocks Problem\",11806,0,1000000000,0,911,0,0,12366,0,19363,6,8576,200,20990,5738,17518,3000,1,0],[38,102,\"Ecological Bin Packing\",21625,0,1000000000,0,1948,0,0,11862,0,4418,6,2810,72,32599,640,33663,3000,1,0],[39,103,\"Stacking Boxes\",6369,0,1000000000,0,36,0,0,9101,0,4407,0,2869,0,11452,1351,9536,3000,2,0],[40,104,\"Arbitrage\",3711,10,1000000000,0,306,0,0,2238,0,2205,5,3538,427,12052,578,6228,3000,2,0]]";
+const QByteArray problemData = "[[40,104,\"this \\\"is\\\" [a] test\",3711,10,1000000000,0,306,0,0,2238,0,2205,5,3538,427,12052,578,6228,3000,2,0],[36,100,\"The 3n + 1 problem\",69204,7,1000000000,0,6669,0,0,102913,0,57933,146,53082,5209,242535,4512,174208,3000,1,0],[37,101,\"The Blocks Problem\",11806,0,1000000000,0,911,0,0,12366,0,19363,6,8576,200,20990,5738,17518,3000,1,0],[38,102,\"Ecological Bin Packing\",21625,0,1000000000,0,1948,0,0,11862,0,4418,6,2810,72,32599,640,33663,3000,1,0],[39,103,\"Stacking Boxes\",6369,0,1000000000,0,36,0,0,9101,0,4407,0,2869,0,11452,1351,9536,3000,2,0],[40,104,\"Arbitrage\",3711,10,1000000000,0,306,0,0,2238,0,2205,5,3538,427,12052,578,6228,3000,2,0]]";
+const QByteArray judgeData = "[{\"id\":1433402181069, \"type\":\"lastsubs\", \"msg\":{\"sid\":15735294,\"uid\":159399,\"pid\":481,\"ver\":0,\"lan\":2,\"run\":0,\"mem\":0,\"rank\":-1,\"sbt\":1436337218,\"name\":\"Jim\",\"uname\":\"vjudge10\"}},{\"id\":1433402181070, \"type\":\"lastsubs\", \"msg\":{\"sid\":15735292,\"uid\":159395,\"pid\":1876,\"ver\":80,\"lan\":5,\"run\":0,\"mem\":0,\"rank\":-1,\"sbt\":1436337213,\"name\":\"Tom\",\"uname\":\"vjudge6\"}},{\"id\":1433402181071, \"type\":\"lastsubs\", \"msg\":{\"sid\":15735293,\"uid\":705089,\"pid\":318,\"ver\":70,\"lan\":1,\"run\":0,\"mem\":0,\"rank\":-1,\"sbt\":1436337215,\"name\":\"Safial Islam Ayon\",\"uname\":\"safialislam302\"}}]";
+
+void showProblemList(QList<uva::ProblemInfo> data)
+{
+    cout << "Problem List data: "
+         << data.count() << " items" << endl;
+    for (uva::ProblemInfo info : data)
+    {
+        cout << info.getProblemNumber() << " - "
+             << info.getProblemTitle().toStdString() << " "
+             << info.getDACU() << " " << info.getAcceptedCount() << " "
+             << info.getTotalSubmission() << endl;
+    }
+    cout << endl;
+}
+void showJudgeStatus(QList<uva::JudgeStatus> data)
+{
+    cout << "Judge status data: "
+         << data.count() << " items" << endl;
+    for(uva::JudgeStatus stat : data)
+    {
+        cout << stat.getId() << " " << stat.getSubmissionID() << " "
+             << stat.getFullName().toStdString() << " " << stat.getProblemID() << " "
+             << stat.getVerdict() << " " << stat.getLanguage() << " "
+             << stat.getSubmissionTime() << endl;
+    }
+    cout << endl;
+}
 
 void TestSampleData(uva::Uhuntqt& api)
 {
-    cout << "Sample data: " << endl;
-    for (uva::ProblemInfo info : api.problemListFromData(sampleData))
-    {
-        cout << info.getProblemNumber() << " - "
-             << info.getProblemTitle().toStdString() << endl;
-    }
-    cout << endl;
+    //problem list test
+    showProblemList(api.problemListFromData(problemData));
+
+    //judge status test
+    showJudgeStatus(api.judgeStatusFromData(judgeData));
 }
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
+    //freopen("test.txt", "w", stdout);
+
     shared_ptr<QNetworkAccessManager> manager = make_shared<QNetworkAccessManager>();
     uva::Uhuntqt api(manager);
 
-    QObject::connect(&api, &uva::Uhuntqt::problemListDownloaded, 
-        [] (QList<uva::ProblemInfo> data)
-        {
-            for (uva::ProblemInfo info : data)
-            {
-                cout << info.getProblemNumber() << " - "
-                     << info.getProblemTitle().toStdString() << endl;
-            }
-
-        });
+    //problem list button
+    QObject::connect(&api, &uva::Uhuntqt::problemListDownloaded, &showProblemList);
 
     QPushButton getProblemListButton;
     getProblemListButton.setText("Click me to get the problem list");
 
     QObject::connect(&getProblemListButton, &QPushButton::clicked,
                      &api, &uva::Uhuntqt::getProblemList);
-
     getProblemListButton.show();
+
+    //judge status button
+    QObject::connect(&api, &uva::Uhuntqt::judgeStatusDownloaded, &showJudgeStatus);
+
+    QPushButton getJudgeStatusButton;
+    getJudgeStatusButton.setText("Click me to get the judge status list");
+
+    QObject::connect(&getJudgeStatusButton, &QPushButton::clicked,
+                     &api, &uva::Uhuntqt::getJudgeStatus);
+    getJudgeStatusButton.show();
+
+    //sample test
     TestSampleData(api);
 
     return app.exec();

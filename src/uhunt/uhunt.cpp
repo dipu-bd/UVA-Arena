e6f18d22af81898669873c25a5b3d862174df2a4
jgcoded <juliog@knights.ucf.edu>
2015-07-10 00:37:53 -0400

    uhunt: refactored and renamed uhunt.h/cpp, problem.h/cpp, added get problem by it's id in the Uhunt class, and changed the implementation of getProblemList

---
+++ src/uhunt/uhunt.cpp
@@ -0,0 +1,275 @@
+#include <QFile>
+#include <QDataStream>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+
+#include "uhunt.h"
+
+using namespace uva;
+
+const QString API_BASE = "http://uhunt.felix-halim.net/api";
+const QString API_PROBLEM_LIST = API_BASE + "/p";
+const QString API_PROBLEM_BY_ID = API_BASE + "/p/id/%1";            //%1 = problem id
+const QString API_JUDGE_STATUS = API_BASE + "/poll/%1";             //%1 = userId
+const QString API_USER_NAME_TO_ID = API_BASE + "/uname2uid/%1";     //%1 = userName
+const QString API_USER_INFO = API_BASE + "/subs-user/%1/%2";        //%1 = userId, %2 = lastSubmissionId
+const QString API_RANK_BY_USER = API_BASE + "/ranklist/%1/%2/%3";   //%1 = userId, %2 = nabove, %3 = nbelow
+const QString API_RANK_BY_POS = API_BASE + "/rank/%1/%2";           //%1 = startPos, %2 = count
+
+Uhunt::Uhunt(std::shared_ptr<QNetworkAccessManager> manager) :
+    mNetworkManager(manager)
+{
+}
+
+QNetworkReply* Uhunt::createNetworkRequest(QString url)
+{
+    if (!mNetworkManager)
+        return nullptr;
+
+    QNetworkRequest request;
+    request.setUrl(QUrl(url));
+
+    QNetworkReply* reply = mNetworkManager->get(request);
+
+    return reply;
+}
+
+//
+// SLOTS
+//
+void Uhunt::getProblemById(int id)
+{
+    QNetworkReply* reply = createNetworkRequest(API_PROBLEM_BY_ID.arg(id));
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply]() {
+            if (reply->error() == QNetworkReply::NoError)
+            {
+                emit problemByIdDownloaded(
+                        Problem::fromJson(
+                            reply->readAll()
+                        )
+                    );
+            }
+        });
+}
+
+void Uhunt::getProblemList()
+{
+    if (!mNetworkManager)
+        return;
+
+    QNetworkReply* reply = createNetworkRequest(API_PROBLEM_LIST);
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply]() {
+            if (reply->error() == QNetworkReply::NoError)
+            {
+                emit problemListDownloaded(
+                    this->problemListFromData(reply->readAll())
+                    );
+            }
+
+            reply->deleteLater();
+        });
+}
+
+void Uhunt::getJudgeStatus(int lastSubmissionID)
+{
+    if (!mNetworkManager)
+        return;
+
+    QNetworkReply* reply = createNetworkRequest(API_JUDGE_STATUS.arg(lastSubmissionID));
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply]() {
+            emit this->judgeStatusDownloaded(
+                        this->judgeStatusFromData(reply->readAll())
+                        );
+            reply->deleteLater();
+        });
+}
+
+void Uhunt::getUserID(const QString &userName)
+{
+    if (!mNetworkManager)
+        return;
+
+    QNetworkReply* reply = createNetworkRequest(API_USER_NAME_TO_ID.arg(userName));
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply, userName]() {
+            int id = reply->readAll().toInt();
+            emit this->userIdDownloaded(userName, id);
+            reply->deleteLater();
+        });
+}
+
+void Uhunt::getUserInfo(int userId)
+{
+    if (!mNetworkManager)
+        return;
+
+    QNetworkReply* reply = createNetworkRequest(API_USER_INFO.arg(userId).arg(0));
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply, userId]() {
+            UserInfo uinfo(reply->readAll());
+            uinfo.setUserId(userId);
+            emit this->userInfoDownloaded(uinfo);
+            reply->deleteLater();
+        });
+}
+
+void Uhunt::updateUserInfo(const UserInfo &uinfo)
+{
+    if (!mNetworkManager)
+        return;
+
+    QNetworkReply* reply =
+        createNetworkRequest(API_USER_INFO.arg(uinfo.getUserId()).arg(uinfo.getLastSubmissionID()));
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply]() {
+            UserInfo info;
+            info.loadData(reply->readAll());
+            emit this->userInfoUpdated(info);
+            reply->deleteLater();
+        });
+}
+
+void Uhunt::getRankByUser(int userId, int nAbove, int nBelow)
+{
+    if (!mNetworkManager)
+        return;
+
+    QNetworkReply* reply =
+        createNetworkRequest(API_RANK_BY_USER.arg(userId).arg(nAbove).arg(nBelow));
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply]() {
+            emit this->rankByUserDownloaded(
+                        this->rankListFromData(reply->readAll())
+                    );
+            reply->deleteLater();
+        });
+}
+
+void Uhunt::getRankByPosition(int startPos, int count)
+{
+    if (!mNetworkManager)
+        return;
+
+    QNetworkReply* reply =
+        createNetworkRequest(API_RANK_BY_POS.arg(startPos).arg(count));
+
+    if (reply == nullptr)
+        return;
+
+    QObject::connect(reply,
+        &QNetworkReply::finished,
+        [this, reply]() {
+            emit this->rankByPositionDownloaded(
+                        this->rankListFromData(reply->readAll())
+                    );
+            reply->deleteLater();
+        });
+}
+
+//
+// Other functions
+//
+QList<Problem> Uhunt::problemListFromData(const QByteArray& data)
+{
+    QList<Problem> problems;
+
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+
+    if (!doc.isArray())
+        return QList<Problem>();
+
+    QJsonArray arr = doc.array();
+
+    QJsonArray::const_iterator it = arr.begin();
+
+    while (it != arr.end())
+    {
+        if (it->isArray())
+            problems.push_back(Problem::fromJsonArray(it->toArray()));
+
+        it++;
+    }
+
+    return problems;
+}
+
+QList<JudgeStatus> Uhunt::judgeStatusFromData(const QByteArray &data)
+{
+    /*
+        The data is a javascript array of dictionary of following format:
+        [{...},{...}]
+     */
+    QList<JudgeStatus> status;
+
+    //get a json document from data
+    QJsonDocument jdoc = QJsonDocument::fromJson(data);
+
+    QJsonArray jarr = jdoc.array();
+    for(int i = 0; i < jarr.count(); ++i)
+    {
+        status.push_back(JudgeStatus(jarr[i].toObject()));
+    }
+
+    return status;
+}
+
+QList<RankInfo> Uhunt::rankListFromData(const QByteArray &data)
+{
+    /*
+        Data is a javascript array of objects. Formatted like this-
+        [{...},{...}]
+     */
+    QList<RankInfo> ranks;
+
+    //get json document
+    QJsonDocument& jdoc = QJsonDocument::fromJson(data);
+
+    const QJsonArray& jarr = jdoc.array();
+    for(int i = 0; i < jarr.count(); ++i)
+    {
+        ranks.push_back(RankInfo(jarr[i].toObject()));
+    }
+
+    return ranks;
+}
--- tests/uhuntqttest.cpp
+++ tests/uhuntqttest.cpp
@@ -9,44 +9,51 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 
-#include "uhunt/uhuntqt.h"
+#include "uhunt/uhunt.h"
 
 using namespace std;
+using namespace uva;
 
-
-uva::UserInfo userInfo;
+UserInfo userInfo;
 const QByteArray problemData = "[[40,104,\"this \\\"is\\\" [a] test\",3711,10,1000000000,0,306,0,0,2238,0,2205,5,3538,427,12052,578,6228,3000,2,0],[36,100,\"The 3n + 1 problem\",69204,7,1000000000,0,6669,0,0,102913,0,57933,146,53082,5209,242535,4512,174208,3000,1,0],[37,101,\"The Blocks Problem\",11806,0,1000000000,0,911,0,0,12366,0,19363,6,8576,200,20990,5738,17518,3000,1,0],[38,102,\"Ecological Bin Packing\",21625,0,1000000000,0,1948,0,0,11862,0,4418,6,2810,72,32599,640,33663,3000,1,0],[39,103,\"Stacking Boxes\",6369,0,1000000000,0,36,0,0,9101,0,4407,0,2869,0,11452,1351,9536,3000,2,0],[40,104,\"Arbitrage\",3711,10,1000000000,0,306,0,0,2238,0,2205,5,3538,427,12052,578,6228,3000,2,0]]";
 const QByteArray judgeData = "[{\"id\":1433402181069, \"type\":\"lastsubs\", \"msg\":{\"sid\":15735294,\"uid\":159399,\"pid\":481,\"ver\":0,\"lan\":2,\"run\":0,\"mem\":0,\"rank\":-1,\"sbt\":1436337218,\"name\":\"Jim\",\"uname\":\"vjudge10\"}},{\"id\":1433402181070, \"type\":\"lastsubs\", \"msg\":{\"sid\":15735292,\"uid\":159395,\"pid\":1876,\"ver\":80,\"lan\":5,\"run\":0,\"mem\":0,\"rank\":-1,\"sbt\":1436337213,\"name\":\"Tom\",\"uname\":\"vjudge6\"}},{\"id\":1433402181071, \"type\":\"lastsubs\", \"msg\":{\"sid\":15735293,\"uid\":705089,\"pid\":318,\"ver\":70,\"lan\":1,\"run\":0,\"mem\":0,\"rank\":-1,\"sbt\":1436337215,\"name\":\"Safial Islam Ayon\",\"uname\":\"safialislam302\"}}]";
 const QByteArray dummyUserInfo = "{\"name\":\"Sudipto Chandra\",\"uname\":\"dipu_sust\",\"subs\":[[11555091,96,10,0,1365074607,1,-1],[12112146,382,10,0,1374910525,3,-1],[12112174,382,10,0,1374910892,3,-1],[12113445,382,10,0,1374934476,3,-1],[12113467,382,10,0,1374934867,3,-1],[12115611,382,10,0,1374979816,3,-1],[12115648,382,10,0,1374980298,3,-1]]}";
 const QList<QString> userNames = {"baodog", "dipu_sust", "felix_halim", "invalid-user", "", "-", "jgcoded"};
 const QByteArray rankData = "[{\"rank\":1,\"old\":0,\"userid\":1133,\"name\":\"Josh Bao\",\"username\":\"baodog\",\"ac\":4528,\"nos\":16236,\"activity\":[0,0,24,37,198]},{\"rank\":2,\"old\":0,\"userid\":2223,\"name\":\"try\",\"username\":\"try\",\"ac\":4366,\"nos\":6338,\"activity\":[0,0,0,5,951]},{\"rank\":3,\"old\":0,\"userid\":111636,\"name\":\"Brian Fry\",\"username\":\"brianfry713\",\"ac\":4357,\"nos\":13299,\"activity\":[0,0,2,5,527]},{\"rank\":4,\"old\":0,\"userid\":19304,\"name\":\"Krzysztof Stencel\",\"username\":\"stencel\",\"ac\":4127,\"nos\":9146,\"activity\":[0,3,21,60,264]},{\"rank\":5,\"old\":0,\"userid\":6320,\"name\":\"Lee Wei\",\"username\":\"evandrix\",\"ac\":3511,\"nos\":7519,\"activity\":[0,0,6,28,200]},{\"rank\":6,\"old\":0,\"userid\":2448,\"name\":\"Neal Zane\",\"username\":\"nealzane\",\"ac\":3389,\"nos\":6707,\"activity\":[0,0,12,19,127]},{\"rank\":7,\"old\":0,\"userid\":2397,\"name\":\"dreamoon\",\"username\":\"dreamoon\",\"ac\":3309,\"nos\":10050,\"activity\":[0,0,2,4,107]},{\"rank\":8,\"old\":0,\"userid\":46705,\"name\":\"No English No AC, I'm out.\",\"username\":\"morris821028\",\"ac\":3042,\"nos\":9585,\"activity\":[1,1,16,123,837]},{\"rank\":9,\"old\":0,\"userid\":10845,\"name\":\"nehc\",\"username\":\"nehcdnr\",\"ac\":2481,\"nos\":9056,\"activity\":[2,7,31,90,317]},{\"rank\":10,\"old\":0,\"userid\":94974,\"name\":\"张翼德\",\"username\":\"vjudge2\",\"ac\":2471,\"nos\":63831,\"activity\":[15,43,222,397,970]}]";
 
-void showProblemList(const QList<uva::ProblemInfo>& data)
+void showProblemById(Problem problem)
+{
+    cout << "Downloaded problem: " << problem.getTitle().toStdString() << endl
+        << "Number: " << problem.getNumber() << endl
+        << "Id: " << problem.getID() << endl << endl;
+}
+
+void showProblemList(const QList<Problem> &data)
 {
     cout << "\nProblem List data: "
          << data.count() << " items" << endl;
 
     int cnt = 0;
-    for (uva::ProblemInfo info : data)
+    for (Problem info : data)
     {
         if(cnt++ > 20) break;
         cout << "level=" << info.getLevel()
-             << "\t num=" << info.getProblemNumber()
+             << "\t num=" << info.getNumber()
              << "\t dacu=" << info.getDACU()
              << "\t ac=" << info.getAcceptedCount()
              << "\t total=" << info.getTotalSubmission()
-             << "\t title=" << info.getProblemTitle().toStdString()
+             << "\t title=" << info.getTitle().toStdString()
              << endl;
     }
     cout << "Problem list data shown" << endl;
 }
-void showJudgeStatus(const QList<uva::JudgeStatus>& data)
+void showJudgeStatus(const QList<JudgeStatus>& data)
 {
     cout << "\nJudge status data: "
          << data.count() << " items" << endl;
 
     int cnt = 0;
-    for(uva::JudgeStatus stat : data)
+    for(JudgeStatus stat : data)
     {
         if(cnt++ > 20) break;
         cout << "sid=" << stat.getSubmissionID()
@@ -65,7 +72,7 @@ void showUserID(QString uname, int id)
     cout << uname.toStdString() << " = " << id << endl;
 }
 
-void ShowUserInfo(const uva::UserInfo& uinfo)
+void ShowUserInfo(const UserInfo& uinfo)
 {
     userInfo = uinfo;
 
@@ -81,7 +88,7 @@ void ShowUserInfo(const uva::UserInfo& uinfo)
     int cnt = 0;
     for(int pnum : uinfo.getSubmissionList().keys())
     {
-        const uva::UserSubmission& usub = uinfo.getSubmission(pnum);
+        const UserSubmission& usub = uinfo.getSubmission(pnum);
         if(cnt++ > 20) break;
         cout << "sid=" << usub.getSubmissionID()
              << "\t pid=" << usub.getProblemID()
@@ -94,13 +101,13 @@ void ShowUserInfo(const uva::UserInfo& uinfo)
     cout << "Userinfo data shown" << endl;
 }
 
-void showRankData(const QList<uva::RankInfo>& data)
+void showRankData(const QList<RankInfo>& data)
 {
     cout << "\nShowing RankInfo data: "
          << data.count() << " items" << endl;
 
     int cnt = 0;
-    for(uva::RankInfo info : data)
+    for(RankInfo info : data)
     {
         if(cnt++ > 20) break;
         cout << "rank=" << info.getRank()
@@ -116,7 +123,7 @@ void showRankData(const QList<uva::RankInfo>& data)
     return;
 }
 
-void TestSampleData(uva::UhuntQt& api)
+void TestSampleData(Uhunt& api)
 {
     //problem list test
     showProblemList(api.problemListFromData(problemData));
@@ -125,7 +132,7 @@ void TestSampleData(uva::UhuntQt& api)
     showJudgeStatus(api.judgeStatusFromData(judgeData));
 
     //user info
-    userInfo = uva::UserInfo(dummyUserInfo);
+    userInfo = UserInfo(dummyUserInfo);
     userInfo.setUserId(222248);
     ShowUserInfo(userInfo);
 
@@ -141,15 +148,16 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
 
     shared_ptr<QNetworkAccessManager> manager = make_shared<QNetworkAccessManager>();
-    uva::UhuntQt api(manager);
+    Uhunt api(manager);
 
-    QObject::connect(&api, &uva::UhuntQt::problemListDownloaded, &showProblemList);
-    QObject::connect(&api, &uva::UhuntQt::judgeStatusDownloaded, &showJudgeStatus);
-    QObject::connect(&api, &uva::UhuntQt::userIdDownloaded, &showUserID);
-    QObject::connect(&api, &uva::UhuntQt::userInfoDownloaded, &ShowUserInfo);
-    QObject::connect(&api, &uva::UhuntQt::userInfoUpdated, &ShowUserInfo);
-    QObject::connect(&api, &uva::UhuntQt::rankByPositionDownloaded, &showRankData);
-    QObject::connect(&api, &uva::UhuntQt::rankByUserDownloaded, &showRankData);
+    QObject::connect(&api, &Uhunt::problemByIdDownloaded, &showProblemById);
+    QObject::connect(&api, &Uhunt::problemListDownloaded, &showProblemList);
+    QObject::connect(&api, &Uhunt::judgeStatusDownloaded, &showJudgeStatus);
+    QObject::connect(&api, &Uhunt::userIdDownloaded, &showUserID);
+    QObject::connect(&api, &Uhunt::userInfoDownloaded, &ShowUserInfo);
+    QObject::connect(&api, &Uhunt::userInfoUpdated, &ShowUserInfo);
+    QObject::connect(&api, &Uhunt::rankByPositionDownloaded, &showRankData);
+    QObject::connect(&api, &Uhunt::rankByUserDownloaded, &showRankData);
 
     //sample test
     TestSampleData(api);
@@ -164,6 +172,7 @@ int main(int argc, char* argv[])
     QPushButton pushButton5;
     QPushButton pushButton6;
     QPushButton pushButton7;
+    QPushButton pushButton8;
     verticalLayout.addWidget(&pushButton1);
     verticalLayout.addWidget(&pushButton2);
     verticalLayout.addWidget(&pushButton3);
@@ -171,39 +180,42 @@ int main(int argc, char* argv[])
     verticalLayout.addWidget(&pushButton5);
     verticalLayout.addWidget(&pushButton6);
     verticalLayout.addWidget(&pushButton7);
+    verticalLayout.addWidget(&pushButton8);
     frame.setLayout(&verticalLayout);
     frame.setWindowTitle("UVA Arena Unit Test");
 
     pushButton1.setText("Click here to get userids");
     QObject::connect(&pushButton1, &QPushButton::clicked,
-                     &api, [&]()
-    {
-        cout << "Userids: " << endl;
-        for(QString x : userNames)
-        {
-            api.getUserID(x);
-        }
-    });
+        [&]() {
+            cout << "Userids: " << endl;
+            for(QString x : userNames)
+            {
+                api.getUserID(x);
+            }
+        });
 
     pushButton2.setText("Click here to get judge status");
-    QObject::connect(&pushButton2, &QPushButton::clicked, &api, [&] { api.getJudgeStatus(); });
+    QObject::connect(&pushButton2, &QPushButton::clicked, [&]() { api.getJudgeStatus(); });
 
     pushButton3.setText("Click here to get problem list");
-    QObject::connect(&pushButton3, &QPushButton::clicked, &api, [&] { api.getProblemList(); } );
+    QObject::connect(&pushButton3, &QPushButton::clicked, [&]() { api.getProblemList(); } );
 
     pushButton4.setText("Click here to get user info");
-    QObject::connect(&pushButton4, &QPushButton::clicked, &api, [&] { api.getUserInfo(222248); });
+    QObject::connect(&pushButton4, &QPushButton::clicked, [&]() { api.getUserInfo(222248); });
 
     pushButton5.setText("Click here to update user info");
-    QObject::connect(&pushButton5, &QPushButton::clicked, &api, [&] { api.updateUserInfo(userInfo); });
+    QObject::connect(&pushButton5, &QPushButton::clicked, [&]() { api.updateUserInfo(userInfo); });
 
     pushButton6.setText("Click here to get ranklist of specific user");
-    QObject::connect(&pushButton6, &QPushButton::clicked, &api, [&] { api.getRankByUser(222248); });
+    QObject::connect(&pushButton6, &QPushButton::clicked, [&]() { api.getRankByUser(222248); });
 
     pushButton7.setText("Click here to get ranklist starting from rank-1");
-    QObject::connect(&pushButton7, &QPushButton::clicked, &api, [&] { api.getRankByPosition(1, 20); });
+    QObject::connect(&pushButton7, &QPushButton::clicked, [&]() { api.getRankByPosition(1, 20); });
 
+    pushButton8.setText("Click here to get a problem by it's id");
+    QObject::connect(&pushButton8, &QPushButton::clicked, [&]() { api.getProblemById(36); });
 
     frame.show();
+
     return app.exec();
 }

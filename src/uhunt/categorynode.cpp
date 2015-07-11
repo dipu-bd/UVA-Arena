14ed73a81f6dd1ce5e04179d65e241cda0d9c317
dipu-bd <dipu.sudipta@gmail.com>
2015-07-11 16:13:34 +0600

    CategoryNode finished

--- src/uhunt/categorynode.cpp
+++ src/uhunt/categorynode.cpp
@@ -1,9 +1,51 @@
 #include "categorynode.h"
+#include "uhuntdatabase.h"
 
 using namespace uva;
 
-CategoryNode::CategoryNode(QObject *parent) : QObject(parent)
+CategoryNode::CategoryNode()
 {
 
 }
+
+CategoryNode CategoryNode::fromJsonObject(const QJsonObject& data)
+{
+    CategoryNode node;
+    QJsonArray::const_iterator it;
+
+    node.setName(data["name"].toString());
+    node.setNote(data["note"].toString());
+
+    //add branches
+    const QJsonArray& branch = data["branches"].toArray();
+    for(it = branch.begin(); it != branch.end(); ++it)
+    {
+        CategoryNode b = CategoryNode::fromJsonObject(it->toObject());
+        node.addBranch(b);
+
+        //also add problems from this sub branch to main branch
+        for(Problem p : b.getProblems())
+        {
+            node.addProblem(p);
+        }
+    }
+
+    //add problems
+    const QJsonArray& prob = data["problems"].toArray();
+    for(it = prob.begin(); it != prob.end(); ++it)
+    {
+        const QJsonObject& ob = it->toObject();
+        int pnum = ob["pnum"].toInt();
+        if(UhuntDatabase::isAvaiable())
+        {
+            Problem p = UhuntDatabase::getProblemByNumber(pnum);
+            p.setStar(ob["star"].toBool());
+            p.setNotes(ob["note"].toString());
+            node.addProblem(p);
+        }
+    }
+
+    return node;
+}
+
 

7a019cb6108a0b9be8882275a0f0e6e38bcdfc02
jgcoded <juliog@knights.ucf.edu>
2015-07-12 17:37:16 -0400

    Store problem map in MainWindow and allow UVAArenaWidgets access to the data via a call to mainWindow()

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -26,13 +26,45 @@ namespace uva
                                 QWidget *parent = 0);
         ~MainWindow();
 
+        //get the problem map
+        const Uhunt::ProblemMap& getProblemMap();
+        //get problem number by problem id
+        int getProblemNumberFromId(int problemId);
+        //get problem id from problem number
+        int getProblemIdFromNumber(int problemNumber);
+        //get problem title by problem number
+        QString getProblemTitle(int problemNumber);
+        //get problem by id
+        const Problem& getProblemById(int problemId);
+        //get problem by problem number
+        const Problem& getProblemByNumber(int problemNumber);
+
     public slots:
 
         void onUVAArenaEvent(UVAArenaWidget::UVAArenaEvent, QVariant);
 
+    private slots:
+
+        void onProblemListByteArrayDownloaded(QByteArray data);
+
     private:
 
+        //set the problem map
+        void setProblemMap(Uhunt::ProblemMap problemMap);
+
         void initialize();
+
+        void initializeData();
+
+        void initializeWidgets();
+
+        void loadProblemListFromFile(QString fileName);
+
+        std::unique_ptr<Uhunt::ProblemMap> mProblems;
+
+        std::unique_ptr<QMap<int, int> > mProblemIdToNumber;
+
+        qint64 mMaxDaysUntilProblemListRedownload;
 
         std::vector<UVAArenaWidget*> mUVAArenaWidgets;
 

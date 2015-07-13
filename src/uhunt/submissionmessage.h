37889847a224ecccdc784bae9c02430d12c98d65
jgcoded <juliog@knights.ucf.edu>
2015-07-12 22:11:56 -0400

    SubmissionMessage: standardized documentation on member methods

--- src/uhunt/submissionmessage.h
+++ src/uhunt/submissionmessage.h
@@ -15,155 +15,174 @@ namespace uva
         SubmissionMessage();
 
         /*!
-           \brief Converts the QJsonObject type data into SubmissionMessage object.
-           \param[data] SubmissionMessage data in JSON format.
-           \return A new SubmissionMessage object.
+            \brief Converts the QJsonObject into a SubmissionMessage object.
+
+            \param[in] data SubmissionMessage data in JSON format.
+            \return A new SubmissionMessage object.
          */
         static SubmissionMessage fromJsonObject(const QJsonObject& data);
 
         /*!
-          \brief Gets the id of submission.
+            \brief Gets the id of submission.
          */
         int getSubmissionID() const { return mSubmissionID; }
+
         /*!
            \brief Sets the id of submission.
-           \param[v] ID of Submission.
+           \param[in] id ID of Submission.
          */
-        void setSubmissionID(int v) { mSubmissionID = v; }
+        void setSubmissionID(int id) { mSubmissionID = id; }
 
         /*!
-          \brief Gets the id of user.
+            \brief Gets the id of user.
          */
         int getUserID() const { return mUserID; }
-        /*!
-           \brief Sets the id of user.
-           \param[v] ID of user.
-         */
-        void setUserID(int v) { mUserID = v; }
 
         /*!
-          \brief Gets the id of submitted problem.
+           \brief Sets the id of the user.
+
+           \param[in] id ID of the user.
+         */
+        void setUserID(int id) { mUserID = id; }
+
+        /*!
+            \brief Gets the id of submitted problem.
          */
         int getProblemID() const { return mProblemID; }        
         /*!
-           \brief Sets the id of submitted problem.
-           \param[v] ID of problem.
+            \brief Sets the id of submitted problem.
+
+            \param[in] id ID of problem.
          */
         void setProblemID(int v) { mProblemID = v; }
 
         /*!
-          \brief Gets the verdict of submission.
+            \brief Gets the verdict of submission.
          */
         Verdict getVerdict() const { return mVerdict; }
-        /*!
-           \brief Sets the verdict of submission.
-           \param[v] Verdict.
-         */
-        void setVerdict(Verdict v) { mVerdict = v; }
-        /*!
-           \brief Sets the verdict of submission.
-           \param[v] Verdict.
-         */
-        void setVerdict(int v);
 
         /*!
-          \brief Gets the language of submitted code.
+            \brief Sets the verdict enum value for the submission.
+
+            \param[in] verdict Value from the Verdict enum.
+         */
+        void setVerdict(Verdict verdict) { mVerdict = verdict; }
+
+        /*!
+            \brief Sets the verdict of the submission.
+            \param[in] verict Value from the Verdict enum.
+         */
+        void setVerdict(int verdict);
+
+        /*!
+            \brief Gets the programming language of the submitted code.
          */
         Language getLanguage() const { return mLanguage; }
+
         /*!
-           \brief Sets the language of submitted code.
-           \param[v] Language.
+            \brief Sets the programming language of the submitted code.
+
+            \param[in] language Programming language used for the submission.
          */
-        void setLanguage(Language v) { mLanguage = v; }
+        void setLanguage(Language language) { mLanguage = language; }
+
         /*!
-           \brief Sets the language of submitted code.
-           \param[v] Language.
+            \brief Sets the programming language of the submitted code.
+
+            \param[in] language Programming language used for the submission.
          */
         void setLanguage(int v);
 
         /*!
-          \brief Gets the runtime in milliseconds of submission.
+            \brief Gets the runtime in milliseconds of the submission.
          */
         int getRuntime() const { return mRuntime; }
-        /*!
-           \brief Sets the runtime of submission.
-           \param[v] Runtime in milliseconds.
-         */
-        void setRuntime(int v);
 
         /*!
-          \brief Gets the memory needed in bytes on runtime in this submission.
-                 WARNING: This value is no longer supported by UVA OJ.
+            \brief Sets the runtime of the submission.
+            \param[in] runtime Runtime in milliseconds.
+         */
+        void setRuntime(int runtime);
+
+        /*!
+            \brief Gets the memory needed in bytes of the runtime for this submission.
+                   WARNING: This value is no longer supported by UVA OJ.
          */
         int getMemory() const { return mMemory; }
-        /*!
-           \brief Sets the memory needed on runtime in this submission.
-           \param[v] Memory in bytes.
-         */
-        void setMemory(int v);
 
         /*!
-          \brief Gets the rank of the user in this submission.
+            \brief Sets the memory needed on runtime in this submission.
+            \param[in] memory Memory in bytes.
+         */
+        void setMemory(int memory);
+
+        /*!
+            \brief Gets the rank of the user for this submission.
          */
         int getRank() const { return mRank; }
-        /*!
-           \brief Sets the rank of the user in this submission.
-           \param[v] Rank of user.
-         */
-        void setRank(int v) { mRank = v; }
 
         /*!
-          \brief Gets the time of submission in UNIX timestamp.
+            \brief Sets the rank of the user in this submission.
+            \param[in] rank Rank of user.
          */
-        int getSubmissionTime() const { return vSubmissionTime; }
+        void setRank(int rank) { mRank = rank; }
+
+        /*!
+            \brief Gets the time of submission in UNIX timestamp.
+         */
+        int getSubmissionTime() const { return mSubmissionTime; }
+
         /*!
            \brief Sets the time of submission in UNIX timestamp.
-           \param[v] Time in UNIX timestamp.
+           \param[in] time Time in UNIX timestamp.
          */
-        void setSubmissionTime(int v) { vSubmissionTime = v; }
+        void setSubmissionTime(int time) { mSubmissionTime = time; }
 
         /*!
-          \brief Gets the full name of the user.
+            \brief Gets the full name of the user.
          */
         QString getFullName() const { return mFullName; }
-        /*!
-           \brief Sets the full name of the user.
-           \param[v] Full name of the user.
-         */
-        void setFullName(QString v) { mFullName = v; }
 
         /*!
-          \brief Gets the user name.
+            \brief Sets the full name of the user.
+            \param[in] fullName Full name of the user.
+         */
+        void setFullName(QString fullName) { mFullName = fullName; }
+
+        /*!
+            \brief Gets the user name.
          */
         QString getUserName() const { return mUserName; }
+
         /*!
-           \brief Sets the user name.
-           \param[v] User name.
+            \brief Sets the user name.
+            \param[in] userName The user name.
          */
-        void setUserName(QString v) { mUserName = v; }
-
+        void setUserName(QString userName) { mUserName = userName; }
 
         /*!
-          \brief Gets the number of submitted problem.
-                 REQUIRES: UvaArenaWidget to be avaiable.
+            \brief Gets the number of submitted problem.
+                   REQUIRES: UvaArenaWidget to be avaiable.
          */
         int getProblemNumber() const { return mProblemNumber; }
-        /*!
-           \brief Sets the number of submitted problem.
-           \param[v] Problem number.
-         */
-        void setProblemNumber(int v) { mProblemNumber = v; }
 
         /*!
-          \brief Gets the title of the submitted problem.
-                 REQUIRES: UvaArenaWidget to be avaiable.
+            \brief Sets the number of submitted problem.
+            \param[in] problemNumber Problem number.
+         */
+        void setProblemNumber(int problemNumber) { mProblemNumber = problemNumber; }
+
+        /*!
+            \brief Gets the title of the submitted problem.
+                   REQUIRES: UvaArenaWidget to be avaiable.
          */
         QString getProblemTitle() const { return mProblemTitle; }
+
         /*!
-           \brief Sets the title of the submitted problem.
-           \param[v] Problem title.
+            \brief Sets the title of the submitted problem.
+            \param[in] problemTitle Problem title.
          */
-        void setProblemTitle(QString v) { mProblemTitle = v; }
+        void setProblemTitle(QString problemTitle) { mProblemTitle = problemTitle; }
 
     private:
         //sid: Submission ID
@@ -183,7 +202,7 @@ namespace uva
         //rank: Submission Rank
         int mRank;
         //sbt: Submission Time (UNIX time stamp)
-        int vSubmissionTime;
+        int mSubmissionTime;
         //name: full username
         QString mFullName;
         //uname: user name
